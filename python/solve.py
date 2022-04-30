433e632c4f51439b0163870900ba5fadb64874a2
Joshua Lin <jasslin2002@gmail.com>
2022-04-29 18:01:34 -0700

    Co-authored-by: Alex Liang <Aheadboat@users.noreply.github.com> Co-authored-by: Evanatious <Evanatious@users.noreply.github.com>

--- python/solve.py
+++ python/solve.py
@@ -10,7 +10,7 @@ import sys
 from typing import Callable, Dict
 import numpy as np
 from gekko import GEKKO
-
+from point import Point
 from instance import Instance
 from solution import Solution
 from file_wrappers import StdinFileWrapper, StdoutFileWrapper
@@ -27,50 +27,89 @@ def solve_naive(instance: Instance) -> Solution:
 @return: Solution
 """
 def solve_GEKKO(instance: Instance) -> Solution:
-    m = GEKKO()
+    #given variables
+    D = instance.grid_side_length
+    Rp = instance.penalty_radius
+    Rs = instance.coverage_radius
+    cities = instance.cities
+    m = GEKKO(remote=False)
     #DxD Variable Array (representative of graph)
     v = m.Array(m.Var, (instance.grid_side_length, instance.grid_side_length),lb=0,ub=1,integer=True)
     #Initial Guess (1 on each city, 0 everywhere else)
     for i in range(instance.grid_side_length):
         for j in range(instance.grid_side_length):
             v[i,j].value = 0
+            v[i,j].upper = 1
+            v[i,j].lower = 0
 
     for c in instance.cities:
         v[c.x,c.y].value = 1
-
+        v[i,j].upper = 1
+        v[i,j].lower = 0
     #define parameter
-    one = m.Param(value=1)
+    one = m.Param(value = 1)
+    zero = m.Param(value = 0)
+
     
     #Objective Function: multiple functions are added together
-    #   Penalty equation: (ln(170) + .17(sum of all towers within penalty range)) * x(ij)
-    
+    #   Penalty equation: (ln(170) + .17(sum of all towers within penalty range)) * x(ij)  
 
+    #goes through every single possible tower placement
+    for x in range(D):
+        for y in range(D):
+            w = []
+            #goes through all points in the general area
+            for i in range(max(0, x-Rp), min(Rp, x+Rp)):
+                for j in range(max(0, y-Rp), min(Rp, y+Rp)):        
+                    if (Point(i,j).distance_obj(Point(x,y)) <= Rp):
+                        w.append(v[i,j])
+            m.Minimize((170 * 2.7182 ** (.17 * m.sum(w))) * v[x,y])
     
     
     #Constraints; equation format: m.Equation(m.sum([xi**2 for xi in x])==40); m.Equation(np.prod(x) >= 25; x1 + x2 == 20)
     #at least one point within 3 of each city must be at least 1
-
+    
     #instantiate np array of size(cities)
-    equations = np.array([0 for i in range(len(instance.cities))])
+    #use gekko
+    #equations = np.array([0 for i in range(len(instance.cities))])
 
-    biglist=[]
+    #Make sure every city is covered by at least one tower
     for c in instance.cities:
         x,y=c.x,c.y
-        l=[]
-        for i in range(max(0, x-3), min(instance.grid_side_length, x+3)):
-            for j in range(max(0, y-3), min(instance.grid_side_length, y+3)):
+        l= []
+        for i in range(max(0, x-instance.coverage_radius), min(instance.grid_side_length, x+instance.coverage_radius)):
+            for j in range(max(0, y-instance.coverage_radius), min(instance.grid_side_length, y+instance.coverage_radius)):
                 #if i+j<=3 or (abs(i-x) == abs(j-y) and (abs(i-x) == 2 or abs(j-y) == 2)):
                 #    l.append(v[i,j]) TODO: Fix this so that it's using dist instead
-                i=1
+                if (c.distance_obj(Point(i, j)) <= instance.coverage_radius):
+                    l.append(v[i,j])
+        m.Equation(m.sum(l) >= one)
 
-        biglist.append(l)
+    #set solver to MINLP
+    m.options.SOLVER = 1
+    #Run solver
+    m.solve()
 
-    for i in range(len(biglist)):
-        equations[i] = m.Equation(np.sum(biglist[i]) >= one)   
+    #solution list
+    sol = []
+    #return towers
+    points_in_radius = 29
+    for x in range(D):
+        for y in range(D):
+            if v[x,y].value[0] >= (1/points_in_radius):
+                print(f"Tower exists at ({x},{y}): {v[x,y].value}")
+                sol.append(Point(x,y))
+    return Solution(
+        instance = instance,
+        towers = sol
+    )
 
-    #uses gekko Equations() function to instantiate the np array into satisfactory Equation objects
-    equations = m.Equations(equations)
 
+def solve_gurobi(instance: Instance) -> Solution:
+    return Solution(
+        instance = instance,
+        towers = instance.cities
+    )
 
 
 
@@ -79,7 +118,8 @@ def solve_GEKKO(instance: Instance) -> Solution:
 
 
 SOLVERS: Dict[str, Callable[[Instance], Solution]] = {
-    "naive": solve_naive
+    "naive": solve_naive,
+    "GEKKO": solve_GEKKO
 }
 
 
